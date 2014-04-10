a7d5362ad6c1d3b8222562afe05ab3cb5ea6a564
Furkan Mustafa <f@s2n.io>
2014-04-10 20:41:16 +0900

    FMHTTP\Request::InitDefault, gets the current request made to the web server

--- Request.php
+++ Request.php
@@ -4,18 +4,23 @@ namespace FMHTTP;
 
 class Request extends Base {
 	public $url;
+	public $uri = null;
+	public $files = null;
+	public $data = null;
 	public $method = 'GET';
 	public $postData = null;
 	public $timeout = null;
 	
-	public function __construct($url = null) {
+	static $Request = null;
+	
+	function __construct($url = null) {
 		if ($url)
 			$this->url = $url;
 	}
-	public function setJSON() {
+	function setJSON() {
 		$this->setHeader('Content-Type', 'application/json');
 	}
-	public function &send() {
+	function &send() {
 		if (!$this->url)
 			return false;
 		$opts['http']['method'] = $this->method;
@@ -68,12 +73,47 @@ class Request extends Base {
 		
 		return $response;
 	}
-}
-
-// forgive me for the ugliness below. not my fault.
-
-if (!function_exists('getallheaders')) {
-	function getallheaders() {
+	
+	function setBody($bodyString) {
+		$this->body = $bodyString;
+		
+		// .. process depending on content-type
+		
+		// do this later:
+		// Content-Type	multipart/form-data; boundary=----WebKitFormBoundaryp6D5nfMyUxAWWBwr
+		//
+		if (count($_POST) || !isset($this->headers['Content-Type'])) {
+			$this->data = $_POST;
+		} else if ($this->headers['Content-Type']==='application/json') {
+			$this->data = json_decode($this->body, true);
+		}
+	}
+	
+	static function InitCurrent() {
+		if (self::$Request)
+			return self::$Request;
+		
+		self::$Request = new Request();
+		if (php_sapi_name() === 'cli') {
+			self::$Request->setBody(stream_get_contents(STDIN));
+			self::$Request->method = 'CLI';
+		} else if (isset($_SERVER['REQUEST_METHOD'])) {
+			self::$Request->uri = $_SERVER['REQUEST_URI'];
+			self::$Request->method = $_SERVER['REQUEST_METHOD'];
+			self::$Request->headers = self::__getAllHeaders();
+			if (!$_FILES) {
+				self::$Request->setBody(file_get_contents('php://input'));
+			} else {
+				self::$Request->files = $_FILES;
+			}
+		}
+		return self::$Request;
+	}
+	
+	static private function __getAllHeaders() {
+		if (function_exists('getallheaders'))
+			return getallheaders();
+		
 		$headers = '';
 		foreach ($_SERVER as $name => $value) {
 			if (substr($name, 0, 5) == 'HTTP_') {
@@ -83,3 +123,5 @@ if (!function_exists('getallheaders')) {
 		return $headers;
 	}
 }
+
+Request::InitCurrent();
